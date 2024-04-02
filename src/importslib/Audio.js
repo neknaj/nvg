8e355dd17e6cc5199c8ac176c1cf71a09219e25d
bem130 <haruki070617@gmail.com>
2024-04-03 02:45:57 +0900

    change a little

--- src/importslib/Audio.js
+++ src/importslib/Audio.js
@@ -4,6 +4,60 @@ function init() {
 
 
 const val = {
+    mono2stereo: (_,args)=>{
+        const [inputbuffer,bias] = args;
+        const l = bias?.[0]??1
+        const r = bias?.[1]??1
+        const length = inputbuffer.length;
+        const outputbuffer = new AudioBuffer({
+            numberOfChannels: 2,
+            length: length,
+            sampleRate: audioCtx.sampleRate,
+        });
+        const output1 = outputbuffer.getChannelData(0);
+        const output2 = outputbuffer.getChannelData(1);
+        const input = inputbuffer.getChannelData(0);
+        for (let i = 0; i < length; i++) {
+            output1[i] += input[i]*r;
+            output2[i] += input[i]*l;
+        }
+        return outputbuffer;
+    },
+    volume: (_,args)=>{
+        const [inputbuffer,volume] = args;
+        const length = inputbuffer.length;
+        const outputbuffer = new AudioBuffer({
+            numberOfChannels: inputbuffer.numberOfChannels,
+            length: length,
+            sampleRate: audioCtx.sampleRate,
+        });
+        for (let c=0;c<inputbuffer.numberOfChannels;c++) {
+            const output = outputbuffer.getChannelData(c);
+            const input = inputbuffer.getChannelData(c);
+            for (let i = 0; i < length; i++) {
+                output[i] += input[i]*volume;
+            }
+        }
+        return outputbuffer;
+    },
+    biasedVolume: (_,args)=>{
+        const [inputbuffer,bias] = args;
+        const length = inputbuffer.length;
+        const outputbuffer = new AudioBuffer({
+            numberOfChannels: inputbuffer.numberOfChannels,
+            length: length,
+            sampleRate: audioCtx.sampleRate,
+        });
+        for (let c=0;c<inputbuffer.numberOfChannels;c++) {
+            const output = outputbuffer.getChannelData(c);
+            const input = inputbuffer.getChannelData(c);
+            const volume = bias[c]??1;
+            for (let i = 0; i < length; i++) {
+                output[i] += input[i]*volume;
+            }
+        }
+        return outputbuffer;
+    },
     Get: async(_,args)=>{
         return await getAudioBuffer(args[0]);
     },
