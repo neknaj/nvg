1d6d9476b9eb047fc3cc127e59ac3438258c313e
bem130 <haruki070617@gmail.com>
2024-04-02 17:46:39 +0900

    change a little

--- src/importslib/Canvas.js
+++ src/importslib/Canvas.js
@@ -7,8 +7,8 @@ const val = {
         const [image,x,y,scale] = args;
         const canvas = document.createElement("canvas");
         const canvasctx = canvas.getContext("2d");
-        canvas.width = Project.scope.size.width;
-        canvas.height = Project.scope.size.height;
+        canvas.width = Project.scope.__size.width;
+        canvas.height = Project.scope.__size.height;
         canvasctx.scale(scale.x,scale.y);
         canvasctx.drawImage(image,x,y,image.width*scale.x,image.height*scale.y);
         return canvas;
@@ -18,8 +18,8 @@ const val = {
         const canvas = document.createElement("canvas");
         const canvasctx = canvas.getContext("2d");
         video.currentTime = args[4];
-        canvas.width = Project.scope.size.width;
-        canvas.height = Project.scope.size.height;
+        canvas.width = Project.scope.__size.width;
+        canvas.height = Project.scope.__size.height;
         canvasctx.scale(args[3].x,args[3].y);
         canvasctx.drawImage(video,args[1],args[2],video.videoWidth*args[3].x,video.videoHeight*args[3].y);
         return canvas;
